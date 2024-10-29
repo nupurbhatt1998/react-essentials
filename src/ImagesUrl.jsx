9a3da2a515771fa4c510e89807998c89988e1ae4
Nupur Bhatt <nupurbhatt2014@gmail.com>
2024-10-29 12:00:21 +0530

    Third Commit: added images and image url files

---
+++ src/ImagesUrl.jsx
@@ -0,0 +1,13 @@
+import Components from './assets/components.png';
+import Config from './assets/config.png';
+import Jsxui from './assets/jsx-ui.png';
+import ReactCore from './assets/react-core-concepts.png';
+import StateManagement from './assets/state-mgmt.png';
+
+export const Images = {
+   Components,
+   Config,
+   Jsxui,
+   ReactCore,
+   StateManagement
+}
